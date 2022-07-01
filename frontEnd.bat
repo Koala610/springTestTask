53531fab21a6b91e5135caa5afea320bf9d77602
Vladlen <vladli2000610@gmail.com>
2022-07-02 00:25:11 +0600

    Changed script

--- frontEnd.bat
+++ frontEnd.bat
@@ -1,3 +1,9 @@
 cd frontEnd
 call npm install
+cd dist/eschool
+if not exist index.html call ng build
+cd ..
+cd ..
+set /p ANGULAR_PORT="Enter server port: "
+echo Starting server...
 node server.js
